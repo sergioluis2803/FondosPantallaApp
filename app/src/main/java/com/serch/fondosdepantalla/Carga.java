c9e9b48a11da4319c719fc33c92cc86cd7945afc
sergioluis2803 <sergiomv2803@gmail.com>
2024-02-28 23:37:19 -0500

    personalizacion de pantalla de carga

--- app/src/main/java/com/serch/fondosdepantalla/Carga.java
+++ app/src/main/java/com/serch/fondosdepantalla/Carga.java
@@ -3,15 +3,25 @@ package com.serch.fondosdepantalla;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
 
 public class Carga extends AppCompatActivity {
+
+    TextView app_name, desarrollador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.carga);
+
+        String ubicacion = "fuentes/sans_negrita.ttf";
+        Typeface tf = Typeface.createFromAsset(Carga.this.getAssets(), ubicacion);
+
+        app_name = findViewById(R.id.app_name);
+        desarrollador = findViewById(R.id.desarrollador);
 
         final int DURACION = 3000;
 
@@ -20,5 +30,8 @@ public class Carga extends AppCompatActivity {
             startActivity(intent);
             finish();
         }, DURACION);
+
+        app_name.setTypeface(tf);
+        desarrollador.setTypeface(tf);
     }
 }
